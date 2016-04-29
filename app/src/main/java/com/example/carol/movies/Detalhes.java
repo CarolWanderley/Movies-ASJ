637ae65e0c391962978167b2e026167a6f3ac842
CarolWanderley <ana.caroline.silva@ccc.ufcg.edu.br>
2016-04-29 14:53:05 -0300

    Ultimos ajustes de codigo

--- app/src/main/java/com/example/carol/movies/Detalhes.java
+++ app/src/main/java/com/example/carol/movies/Detalhes.java
@@ -1,8 +1,12 @@
 package com.example.carol.movies;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +18,10 @@ public class Detalhes extends AppCompatActivity{
     ImageView poster;
     TextView title;
     TextView duracao;
+    TextView nota;
+    TextView descricao;
+    ImageView trailer;
+    Button favorito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +34,37 @@ public class Detalhes extends AppCompatActivity{
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        Bundle bundle = getIntent().getExtras();
-
+        final Bundle bundle = getIntent().getExtras();
 
         poster = (ImageView) findViewById(R.id.poster);
         title = (TextView) findViewById(R.id.titulo);
         duracao = (TextView) findViewById(R.id.duracao);
+        nota = (TextView) findViewById(R.id.rating);
+        descricao = (TextView) findViewById(R.id.descricao);
+        trailer = (ImageView) findViewById(R.id.trailer);
+        favorito = (Button) findViewById(R.id.favorite);
 
         poster.setImageResource(bundle.getInt("Poster"));
         title.setText(bundle.getString("Titulo") + " (" + bundle.getString("Ano") + ")");
         duracao.setText(bundle.getString("Duracao"));
+        nota.setText(bundle.getString("Nota"));
+        descricao.setText(bundle.getInt("Descricao"));
+        trailer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(bundle.getString("Trailer")));
+                startActivity(intent);
+            }
+        });
+        favorito.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               favorito.setSelected(true);
+            }
+        });
+
+
 
     }
 
