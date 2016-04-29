706a33040a6765b6789dadc8236827a98964dbcb
CarolWanderley <ana.caroline.silva@ccc.ufcg.edu.br>
2016-04-29 10:26:55 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Movies-ASJ: repositorio contendo projeto de conclus∆o do Android Study Jams 2016 - Ana Caroline Wanderley Silva" 
---
+++ app/src/main/java/com/example/carol/movies/Detalhes.java
@@ -0,0 +1,16 @@
+package com.example.carol.movies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Carol on 29/04/2016.
+ */
+public class Detalhes extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}
