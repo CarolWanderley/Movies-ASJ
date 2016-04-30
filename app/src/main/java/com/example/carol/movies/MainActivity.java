80ab38128b064e3a1cb9be6975ad17a3a2393d84
CarolWanderley <ana.caroline.silva@ccc.ufcg.edu.br>
2016-04-29 23:10:51 -0300

    Concluido

--- app/src/main/java/com/example/carol/movies/MainActivity.java
+++ app/src/main/java/com/example/carol/movies/MainActivity.java
@@ -29,11 +29,13 @@ public class MainActivity extends AppCompatActivity {
         filmes = new ArrayList<>();
         posters = new ArrayList<>();
         filmes.add(new Movie("American Psycho", "2000", "1h 42min", "7,6", "https://www.youtube.com/watch?v=5YnGhW4UEhc", R.string.americansinopse,  R.drawable.american));
-        filmes.add(new Movie("Precisamos falar sobre o Kevin", "2011", "1h 52min", "7,5", "https://www.youtube.com/watch?v=ZLRgAe2jLaw", R.string.kevinsinopse,  R.drawable.kevin));
         filmes.add(new Movie("Hannibal", "2001", "2h 11min", "6,8", "https://www.youtube.com/watch?v=eHSYth2wSEk", R.string.hannibalsinopse,  R.drawable.hannibal));
         filmes.add(new Movie("O dragão vermelho", "2002", "2h 4min", "7,2", "https://www.youtube.com/watch?v=f_Iq18xpC6k", R.string.redragonsinopse,  R.drawable.reddragon));
-        filmes.add(new Movie("O silêncio dos inocentes", "1991", "1h 58min", "8,6", "https://www.youtube.com/watch?v=lQKs169Sl0I", R.string.silenciosinopse,  R.drawable.silencio));
         filmes.add(new Movie("León: the professional", "1994", "1h 50min", "8,6", "https://www.youtube.com/watch?v=DcsirofJrlM", R.string.leonsinopse,  R.drawable.leon));
+        filmes.add(new Movie("Precisamos falar sobre o Kevin", "2011", "1h 52min", "7,5", "https://www.youtube.com/watch?v=ZLRgAe2jLaw", R.string.kevinsinopse,  R.drawable.kevin));
+        filmes.add(new Movie("O silêncio dos inocentes", "1991", "1h 58min", "8,6", "https://www.youtube.com/watch?v=lQKs169Sl0I", R.string.silenciosinopse,  R.drawable.silencio));
+
+
         filme1 = (ImageView) findViewById(R.id.filme1);
         filme2 = (ImageView) findViewById(R.id.filme2);
         filme3 = (ImageView) findViewById(R.id.filme3);
